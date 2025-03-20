999ecbf85096cda61157e3de2a12085f60e5a637
JDat <3792125+JDat@users.noreply.github.com>
2025-03-20 14:42:55 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm gridemu
-#gcc -Wall -g -O0 -o gridemu src/*.c -lm -lpthread `pcap-config --cflags --libs` `sdl2-config --cflags --libs`
-gcc -Wall -g -O0 -o gridemu src/*.c -lm -lpthread `sdl2-config --cflags --libs`
